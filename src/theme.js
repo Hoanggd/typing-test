382cde5f753a7d4beac695907c88ff4a5ece1aa0
Hoang <hoang.gd97@gmail.com>
2020-05-27 11:34:47 +0700

    first commit

---
+++ src/theme.js
@@ -0,0 +1,36 @@
+const theme = {
+  dark: {
+    primary: "#1765AD",
+    primaryLighten: "#3C9AE8",
+    secondary: "#D8BD14",
+    background: "#141515",
+    devider: "#1C1F22",
+    modal: "#282E34",
+    border: "#3E4955",
+    title: "rgba(255,255,255,.85)",
+    primaryText: "rgba(255,255,255,.65)",
+    secondaryText: "rgba(255,255,255,.45)",
+    disable: "rgba(255,255,255,.3)",
+    error: "#A61D24",
+    boxShadow: "0 18px 24px rgba(0,0,0,.25)",
+    facebook: "#395185"
+  },
+  light: {
+    primary: "#1890FF",
+    primaryLighten: "#1890FF",
+    secondary: "#FADB14",
+    background: "#fff",
+    devider: "#f5f5f5",
+    modal: "#fff",
+    border: "#DEDEDE",
+    title: "rgba(0,0,0,.85)",
+    primaryText: "rgba(0,0,0,.65)",
+    secondaryText: "rgba(0,0,0,.45)",
+    disable: "rgba(0,0,0,.25)",
+    error: "#A61D24",
+    boxShadow: "0 18px 24px rgba(0,0,0,.25)",
+    facebook: "#395185"
+  }
+}
+
+export default theme
